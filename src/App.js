82ab4878ca50961cf1591f9248ff0cd67a155b06
elowuttke <elowuttke@gmail.com>
2023-03-06 16:26:59 -0300

    class exercises done

--- src/App.js
+++ src/App.js
@@ -4,11 +4,16 @@ import styled from "styled-components";
 export default function App() {
   return (
     <AppContainer>
-      <h1 id="frutinhas">Labefrutas <span role="img" aria-label="frutinhas">🍌 🍉 🍇 🍓 </span></h1>
+      <h1 id="frutinhas">
+        Labefrutas{" "}
+        <span role="img" aria-label="frutinhas">
+          🍌 🍉 🍇 🍓{" "}
+        </span>
+      </h1>
       <Router />
     </AppContainer>
   );
 }
 const AppContainer = styled.div`
-   text-align: center;
+  text-align: center;
 `;
--- src/Pages/Cadastro.js
+++ src/Pages/Cadastro.js
@@ -1,52 +1,58 @@
-import React,{useState} from 'react'
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { handleHome } from "../Router/cordinator";
 import styled from "styled-components";
 
-export default function Cadastro (props){
-  const[formulario, setFormulario]=useState({name:"", url:"", price:""})
+export default function Cadastro({ frutas, setFrutas }) {
   const navigate = useNavigate();
+  const [formulario, setFormulario] = useState({
+    name: "",
+    url: "",
+    price: "",
+  });
 
-  const onChangeInputs=(event)=>{
-    const {name, value}= event.target
-    setFormulario({...formulario, [name]:value})
-  }
-  const handleClick = (event)=>{
-    event.preventDefault()
+  const onChangeInputs = (event) => {
+    const { name, value } = event.target;
+    setFormulario({ ...formulario, [name]: value });
+  };
+  const handleClick = (event) => {
+    event.preventDefault();
     console.log(formulario);
-  }
- 
- 
-  return(
+    // const novaFruta = { ...formulario, id: Date.now() };
+    const novaFruta = { ...formulario, id: frutas.lenght + 1 };
+    setFrutas([...frutas, novaFruta]);
+  };
+
+  return (
     <CadastroContainer>
       <h1>Cadastro</h1>
       <button onClick={() => handleHome(navigate)}>Voltar</button>
-    <FormContainer onSubmit={handleClick}>
-      <Input
-      name="name"
-      value={formulario.name}
-      onChange={onChangeInputs}
-      type="text"
-      placeholder="Nome da Fruta"
-      />
-      <Input 
-      name="url"
-      value={formulario.url}
-      onChange={onChangeInputs}
-      type="text"
-      placeholder="url da imagem"
-      />
-      <Input
-      name="price"
-      value={formulario.price}
-      onChange={onChangeInputs}
-      type="number"
-      placeholder="Preço da Fruta"
-      />
-      <button> Cadastrar </button>
-    </FormContainer>
+      <FormContainer onSubmit={handleClick}>
+        <Input
+          name="name"
+          value={formulario.name}
+          onChange={onChangeInputs}
+          type="text"
+          placeholder="Nome da Fruta"
+        />
+        <Input
+          name="url"
+          value={formulario.url}
+          onChange={onChangeInputs}
+          type="text"
+          placeholder="url da imagem"
+        />
+        <Input
+          name="price"
+          value={formulario.price}
+          onChange={onChangeInputs}
+          type="number"
+          placeholder="Preço da Fruta"
+        />
+        <button> Cadastrar </button>
+      </FormContainer>
     </CadastroContainer>
-  )
+  );
 }
 const CadastroContainer = styled.main`
   display: flex;
@@ -59,8 +65,7 @@ const FormContainer = styled.form`
   align-items: center;
 `;
 const Input = styled.input`
-padding: 10px;
-margin: 8px;
-width:200px;
- 
+  padding: 10px;
+  margin: 8px;
+  width: 200px;
 `;
